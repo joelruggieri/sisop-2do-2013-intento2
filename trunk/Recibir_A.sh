259966491cb67aedc7e0e728853f3e173ca13d1c
sevenap@gmail.com <sevenap@gmail.com@c4fe684c-128e-6445-2f4b-cfa5bba15500>
2013-10-15 20:58:26 +0000

    -Impirmir -r

--- trunk/Recibir_A.sh
+++ trunk/Recibir_A.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-ARRIDIR="/home/joel/Documentos/Sistemas_Operativos/arribos/"
-ACEPDIR="/home/joel/Documentos/Sistemas_Operartivos/aceptados"
-REPODIR="/home/joel/Documentos/Sistemas_Operativos/repositorio"
-RECHDIR="/home/joel/Documentos/Sistemas_Operativos/rechazados/"
-MAEDIR="/home/joel/Documentos/Sistemas_Operativos/maestros/"
-DEMONIO_RUTA="/home/joel/Documentos/Sistemas_Operativos/demonio.sh"
-ARCHIVO_SALAS="salas.mae"
-ARCHIVO_OBRAS="obras.mae"
-RUTA_RESERVAR="/home/joel/TP-SO/Reservar_A.sh"
-RUTA_SALAS="$MAEDIR$ARCHIVO_SALAS"
-RUTA_OBRAS="$MAEDIR$ARCHIVO_OBRAS"
-NUMERO_DE_CICLO=1
+#ARRIDIR="/home/maxi/Prueba/Arribos"
+#ACEPDIR="/home/maxi/Prueba/Aceptados"
+#REPODIR="/home/maxi/Prueba/Repositorio"
+#RECHDIR="/home/maxi/Prueba/Rechazados"
+#MAEDIR="/home/maxi/Prueba/Maestros/"
+#DEMONIO_RUTA="/home/maxi/Prueba/demonio.sh"
+#ARCHIVO_SALAS="salas.mae"
+#ARCHIVO_OBRAS="obras.mae"
+#RUTA_RESERVAR="/home/maxi/Desktop/sisop2/Reservar_A.sh"
+#RUTA_SALAS="$MAEDIR$ARCHIVO_SALAS"
+#RUTA_OBRAS="$MAEDIR$ARCHIVO_OBRAS"
+#NUMERO_DE_CICLO=1
 TIEMPO=60 # en segundos
 
 while (true)
@@ -18,7 +18,7 @@ do
 	perl Grabar_L.pl Recibir_A "Ciclo Numero $NUMERO_DE_CICLO"
 	for archivo in `ls $ARRIDIR`
 	do
-		RUTA_ARCHIVO=$ARRIDIR$archivo
+		RUTA_ARCHIVO=$ARRIDIR/$archivo
 		TIPO_DE_TEXTO="`file $RUTA_ARCHIVO`"
 		INVITADOS_BIEN_FORMADO=` echo $archivo | grep -c '^[^- ]*\.inv$'`
 		RESERVA_BIEN_FORMADO=`echo $archivo | grep -c '^[0-9]\+-[^-]*-[^- ]*$'`
@@ -28,7 +28,7 @@ do
 		if [[ $TIPO_TEXTO -eq 0 || ($RESERVA_BIEN_FORMADO -eq 0 && $INVITADOS_BIEN_FORMADO -eq 0) ]] 
 		then
 			perl Mover_A.pl $RUTA_ARCHIVO $RECHDIR Recibir_A
-			perl Grabar_L.pl Recibir_A "El archivo $archico fue rechazado por ser invalido"
+			perl Grabar_L.pl Recibir_A El archivo $archico fue rechazado por ser invalido
 		elif [[ $TIPO_TEXTO -eq 1 && $RESERVA_BIEN_FORMADO -eq 1 ]]
 		then
 			id=`echo $archivo | cut -d "-" -f 1`
@@ -39,10 +39,10 @@ do
 				if [ $ID_CORREO_VALIDOS_SALA -eq 1 ]
 				then
 					perl Mover_A.pl $RUTA_ARCHIVO $ACEPDIR Recibir_A
-					perl Grabar_L.pl Recibir_A "El archivo $archivo de reservas ha sido aceptado"
+					perl Grabar_L.pl Recibir_A El archivo $archivo de reservas ha sido aceptado
 				else
 					perl Mover_A.pl $RUTA_ARCHIVO $RECHDIR Recibir_A
-					perl Grabar_L.pl Recibir_A "El archivo $archivo fue rechazado por par id de Sala-correo invalido/inexistente"
+					perl Grabar_L.pl Recibir_A El archivo $archivo fue rechazado por par id de Sala-correo invalido/inexistente
 				fi
 			elif [ `expr $id % 2` -eq 1 ]
 			then
@@ -51,17 +51,17 @@ do
 				if [[ $ID_CORREO_VALIDOS_OBRA1 -eq 1 || $ID_CORREO_VALIDOS_OBRA2 -eq 1 ]]
 				then
 					perl Mover_A.pl $RUTA_ARCHIVO $ACEPDIR Recibir_A
-					perl Grabar_L.pl Recibir_A "El archivo $archivo de reservas ha sido aceptado"
+					perl Grabar_L.pl Recibir_A El archivo $archivo de reservas ha sido aceptado
 				else
 					perl Mover_A.pl $RUTA_ARCHIVO $RECHDIR Recibir_A
-					perl Grabar_L.pl Recibir_A "El archivo $archivo fue rechazado por par id de Obra-correo invalido/inexistente"
+					perl Grabar_L.pl Recibir_A El archivo $archivo fue rechazado por par id de Obra-correo invalido/inexistente
 				fi
 			 
 			fi
 		elif [[ $TIPO_TEXTO -eq 1 && $INVITADOS_BIEN_FORMADO -eq 1 ]]	
 		then
 			perl Mover_A.pl $RUTA_ARCHIVO $REPODIR Recibir_A
-			perl Grabar_L.pl Recibir_A "El archivo de invitados  $archivo fue aceptado y movido a $REPODIR" 	
+			perl Grabar_L.pl Recibir_A El archivo de invitados  $archivo fue aceptado y movido a $REPODIR	
 		fi
 		
  	
