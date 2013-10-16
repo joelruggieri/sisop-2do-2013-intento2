cf7d9c0ac5003969dbcad0fd99b86cbc93369097
jonathan.tierno@gmail.com <jonathan.tierno@gmail.com@c4fe684c-128e-6445-2f4b-cfa5bba15500>
2013-10-16 00:11:16 +0000

    Verificacion inicializacion ambiente

--- trunk/Datos/Start_A.sh
+++ trunk/Datos/Start_A.sh
@@ -2,12 +2,51 @@
  #TODO asignar el path donde estan los scripts
  pathMadre=$GRUPO
  params=$# 
- #TODO validar que el sistema este inicializado
+
+function estaAmbienteInicializado {
+	if [[ $retorno -ne 0 ]]; then return 1; fi
+	existeVariable "$GRUPO"
+	existeVariable "$BINDIR"
+	existeVariable "$CONFDIR"
+	existeVariable "$MAEDIR"
+	existeVariable "$ARRIDIR"
+	existeVariable "$DATASIZE"
+	existeVariable "$ACEPDIR"
+	existeVariable "$RECHDIR"
+	existeVariable "$PROCDIR"
+	existeVariable "$REPODIR"
+	existeVariable "$LOGDIR"
+	existeVariable "$LOGEXT"
+	existeVariable "$LOGSIZE"
+	return 0
+}
+
+# Verifica la existencia de una variable
+function existeVariable { # $1: Nombre de variable
+	
+	if [[ $retorno -ne 0 ]]; then return 1; fi
+	
+	if [[ ${1:-existe} == "existe" ]]; then
+		retorno=1
+		return 0
+	fi
+	return 1
+}
+
+function main {
  #echo $params
  if  [ $params -ne 3 ] && [ $params -ne 1 ]; then
 	echo "Numero incorrecto de parametros"
  else
+
+	 retorno=0
 	 archivo="$1"
+	 estaAmbienteInicializado
+	 if [[ $retorno -ne 0 ]]; then	
+		
+		echo "No se encuentra inicializado el ambiente"
+		return 1
+	 fi
 	 if  [ ! -f "$archivo" ] ; then 
 		if [ $params -gt 1 ]; then
 			perl Grabar_L.pl $2 $3 "No se encontro el comando '$1'"
@@ -36,3 +75,7 @@
 		 fi
 	 fi
  fi
+
+}
+
+main
